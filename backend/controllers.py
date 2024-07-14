6956396858ca5a8111a183e443f5c53dc59e9a84
Harshit Singh <89565061+harshitsingh09@users.noreply.github.com>
2024-07-14 20:25:54 +0530

    Adding get and post funtionality

--- backend/controllers.py
+++ backend/controllers.py
@@ -1,14 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask import current_app as app # Alias for the current application
 
 @app.route('/') # Refers to base url path (127.0.0.1:5000)
 def home():
     return "<h2>Welcome to Kanban Board</h2>"
 
-@app.route("/login") # Refers to login url path
+@app.route("/login", methods=["GET","POST"]) # Refers to login url path
 def user_login():
+    if request.method == "POST":
+        uname = request.form.get("uname")
+        pwd = request.form.get("pwd")
+        if uname == "admin" and pwd == "admin":
+            return "Login Success"
+        else:
+            return "Login Failed"
     return render_template("login.html")
 
-@app.route("/signup") # Refers to register url path
+@app.route("/signup", methods=["GET","POST"]) # Refers to register url path
 def user_signup():
     return render_template("signup.html")
